e57d1d3cd89830b4a2e852422271ff525e678b8d
Davi Santos <102557963+DaviSantosDEV@users.noreply.github.com>
2022-05-12 14:17:18 -0300

    Add files via upload

---
+++ Exercicio.js
@@ -0,0 +1,39 @@
+
+let numero = prompt('Dos numeros de 0 a 5. Em qual estou pensado?');
+
+if (numero == 4){
+   console.log('Você acertou,Parabéns!');
+else{
+  console.log ('Voce errou que pena;');
+  alert('Voce errou que pena')}
+
+
+  // EXERCICIO 2
+
+let vel = prompt('Coloque a Velocidade do Carro');
+
+if (multa >= 80);
+let multa = (vel - 80)*5;
+{
+alert('Você esta acima do LImite. A multa é de ${multa}');
+
+}
+else {alert('Você foi Multado. VACILÃO🤢')}
+
+// EXERCICIO 3 
+
+let numero = prompt('Impar ou Par') 
+if (numero%2 == 0){ alert('Par') 
+}else{ alert('Impar')}
+
+//EXERCICIO 4
+let salario = parseInt(prompt)('Qual o salario?')
+
+if(salario){
+let aumento = (salario * 10/100)+salario;
+alert(aumento)
+}else{let aumento2 =(salario*15/100)+salario;
+alert(aumento2)}
+
+
+
